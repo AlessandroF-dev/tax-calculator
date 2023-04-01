fb06749333743e86d3691bfc12f11b647cfabeaa
AlessandroF-dev <dev.alessandro01@gmail.com>
2023-04-01 11:34:39 -0300

    adicionado novos testes

--- src/test/java/br/com/taxApi/taxcalculator/TaxIncomeTaxApplicationTests.java
+++ src/test/java/br/com/taxApi/taxcalculator/TaxIncomeTaxApplicationTests.java
@@ -64,13 +64,9 @@ public class TaxIncomeTaxApplicationTests {
     }
 
     @Test
-    @DisplayName("Should return true if admDTO is empty")
+    @DisplayName("Adam DTO is empty because password is not equals did registered")
     public void shouldReturnTrueIfIsEmpty() {
-        Worker worker = new Worker();
         WorkerAdmDTO admDTO = new WorkerAdmDTO();
-
-        worker.setEmail(request.getEmail());
-        worker.setPassword(request.getPassword());
 
         admDTO.setPassword("PasswordNotEquals");
         admDTO.setEmail(request.getEmail());
@@ -81,11 +77,7 @@ public class TaxIncomeTaxApplicationTests {
     @Test
     @DisplayName("Should return true if admDTO is present")
     public void shouldReturnTrueIfIsPresent() {
-        Worker worker = new Worker();
         WorkerAdmDTO admDTO = new WorkerAdmDTO();
-
-        worker.setEmail(request.getEmail());
-        worker.setPassword(request.getPassword());
 
         admDTO.setPassword(request.getPassword());
         admDTO.setEmail(request.getEmail());
@@ -126,7 +118,7 @@ public class TaxIncomeTaxApplicationTests {
         Tax tax = taxRepository.findTax(request.getSalary());
         double incomeTax = request.getSalary() * tax.getTax();
         assertEquals(incomeTax, request.getSalary() * 0.275);
-        assertNotNull(SecurityUtils.mountMessage(request.getName(), request.getSalary(), incomeTax));
+        assertFalse(SecurityUtils.mountMessage(request.getName(), request.getSalary(), incomeTax).isEmpty());
     }
 
     @Test
@@ -172,5 +164,39 @@ public class TaxIncomeTaxApplicationTests {
         assertNotNull(taxRepository.findTax(request.getSalary()));
         assertNotNull(response);
         assertTrue(BCrypt.checkpw(response.get().getPassword(), request.getPassword()));
+    }
+
+    @Test
+    @DisplayName("The tax was successfully found in database")
+    public void shouldReturnTrueIfDataInDataBaseIsPresentAndEquals() {
+        Tax tax = new Tax();
+        tax.setId(1L);
+        tax.setTax(0.0);
+        tax.setCalculatorTierBegin(0);
+        tax.setCalculatorTierEnd(1903.98);
+
+        assertNotNull(taxRepository.findTax(1000));
+
+        assertEquals(tax.getTax(), taxRepository.findTax(1000).getTax());
+        assertEquals(tax.getCalculatorTierBegin(), taxRepository.findTax(1000).getCalculatorTierBegin());
+        assertEquals(tax.getCalculatorTierEnd(), taxRepository.findTax(1000).getCalculatorTierEnd());
+        assertEquals(tax.getId(), taxRepository.findTax(1000).getId());
+    }
+
+    @Test
+    @DisplayName("The tax was not found in database")
+    public void shouldReturnTrueIfDataInDataBaseIsPresentButNotEquals() {
+        Tax tax = new Tax();
+        tax.setId(2L);
+        tax.setTax(0.075);
+        tax.setCalculatorTierBegin(1903.99);
+        tax.setCalculatorTierEnd(2826.65);
+
+        assertNotNull(taxRepository.findTax(1000));
+
+        assertNotEquals(tax.getTax(), taxRepository.findTax(1000).getTax());
+        assertNotEquals(tax.getCalculatorTierBegin(), taxRepository.findTax(1000).getCalculatorTierBegin());
+        assertNotEquals(tax.getCalculatorTierEnd(), taxRepository.findTax(1000).getCalculatorTierEnd());
+        assertNotEquals(tax.getId(), taxRepository.findTax(1000).getId());
     }
 }
