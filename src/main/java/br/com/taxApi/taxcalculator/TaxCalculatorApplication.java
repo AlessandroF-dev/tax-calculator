d9c54f3fcffc51f097894e6d2f6851a2343d44da
AlessandroF-dev <dev.alessandro01@gmail.com>
2023-06-26 22:13:34 -0300

    service discovery, gateway and migrations

--- src/main/java/br/com/taxApi/taxcalculator/TaxCalculatorApplication.java
+++ src/main/java/br/com/taxApi/taxcalculator/TaxCalculatorApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableCaching
+@EnableEurekaClient
+
 public class TaxCalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaxCalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaxCalculatorApplication.class, args);
+    }
 }
---
+++ src/main/resources/db/changelog/sql/20230626-inserttax-rollback-1.sql
@@ -0,0 +1,5 @@
+DELETE FROM workerdb.tax WHERE id = '1';
+DELETE FROM workerdb.tax WHERE id = '2';
+DELETE FROM workerdb.tax WHERE id = '3';
+DELETE FROM workerdb.tax WHERE id = '4';
+DELETE FROM workerdb.tax WHERE id = '5';
